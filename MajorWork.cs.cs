3d80b031ba532af81c681dc0e1768609291ed470
Maks <maxr2312@gmail.com>
2023-03-17 17:52:48 +0200

    Finish Task number 4

--- MajorWork.cs.cs
+++ MajorWork.cs.cs
@@ -8,12 +8,26 @@ namespace Reshetko_IKM_721Б_Сourse_project
 {
     class MajorWork
     {
+        private string SaveFileName;// ім’я файлу для запису
+        private string OpenFileName;// ім’я файлу для читання
+        public void WriteSaveFileName(string S)// метод запису даних в об'єкт
+        {
+            this.SaveFileName = S;// запам'ятати ім’я файлу для запису
+        }
+        public void WriteOpenFileName(string S)
+        {
+            this.OpenFileName = S;// запам'ятати ім’я файлу для відкриття
+        }
+
+
+
         // Вміст робочого об'єкта
         // Поля
         private System.DateTime TimeBegin; // час початку роботи програми
         private string Data; //вхідні дані
         private string Result; // Поле результату
                                // Методи
+
 
         public void SetTime() // метод запису часу початку роботи програми
         {
